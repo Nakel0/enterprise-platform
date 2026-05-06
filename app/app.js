e3a428832c33f74d415d947d94a5b653fad61f03
Okuleye Olalekan <okuleyeolalekan@Okunleye-Mac.lan>
2026-05-06 16:07:14 -0400

    Kosmani website v1

--- app/app.js
+++ app/app.js
@@ -1,9 +1,29 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("I will be successful and everything will align well for me 🚀");
+
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+
+  else if (req.url === "/style.css") {
+    fs.readFile(path.join(__dirname, "style.css"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
+  }
+
+  else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
 });
 
 server.listen(PORT, () => {
